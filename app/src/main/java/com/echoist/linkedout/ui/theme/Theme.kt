d6ccb750b76ae054f832efe1135f107d75f12046
kkh <kkhyungyung0@naver.com>
2024-05-01 00:23:41 +0900

    fix : Theme
    다크모드일때 부터 개발.
    라이트모드의 색은 전부 임시입니다.

--- app/src/main/java/com/echoist/linkedout/ui/theme/Theme.kt
+++ app/src/main/java/com/echoist/linkedout/ui/theme/Theme.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Color(0xFF252525),
     secondary = PurpleGrey80,
     tertiary = Pink80,
     background = Color(0xFF000000),
