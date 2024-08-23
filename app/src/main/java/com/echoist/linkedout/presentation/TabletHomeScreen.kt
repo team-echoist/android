b360eb432b2717abd73480b18799074f31374973
JunYoung Lee <47595801+Lee-Jun-Young@users.noreply.github.com>
2024-08-23 15:01:47 +0900

    feat: 화면 분할 시 탑바 너비 수정

--- app/src/main/java/com/echoist/linkedout/presentation/TabletHomeScreen.kt
+++ app/src/main/java/com/echoist/linkedout/presentation/TabletHomeScreen.kt
@@ -1,12 +1,17 @@
 package com.echoist.linkedout.presentation
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
