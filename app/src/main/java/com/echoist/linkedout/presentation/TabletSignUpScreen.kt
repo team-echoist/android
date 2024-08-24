391bcca9fd78887578d56098f2b005d5c8f95e2f
JunYoung Lee <47595801+Lee-Jun-Young@users.noreply.github.com>
2024-08-24 23:24:03 +0900

    feat: 회원가입 ui 수정 및 방법 수정

--- app/src/main/java/com/echoist/linkedout/presentation/TabletSignUpScreen.kt
+++ app/src/main/java/com/echoist/linkedout/presentation/TabletSignUpScreen.kt
@@ -18,25 +18,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
@@ -47,9 +45,11 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.echoist.linkedout.page.login.Authentication_6_BottomModal
+import com.echoist.linkedout.page.login.EmailTextField
+import com.echoist.linkedout.page.login.PwTextField
 import com.echoist.linkedout.page.login.SendSignUpFinishedAlert
 import com.echoist.linkedout.ui.theme.LinkedInColor
-import com.echoist.linkedout.ui.theme.LinkedOutTheme
 import com.echoist.linkedout.viewModels.SignUpViewModel
 import kotlinx.coroutines.delay
 
@@ -61,15 +61,17 @@ fun TabletSignUpRoute(
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
     val configuration = LocalConfiguration.current
+    val screenHeightDp = configuration.screenHeightDp
+    val passwordFocusRequester = remember { FocusRequester() }
 
-    val bottomSheetState = rememberStandardBottomSheetState(initialValue = SheetValue.Hidden, skipHiddenState = false)
-    val scaffoldState = androidx.compose.material3.rememberBottomSheetScaffoldState(
+    val bottomSheetState =
+        rememberStandardBottomSheetState(initialValue = SheetValue.Hidden, skipHiddenState = false)
+    val scaffoldState = rememberBottomSheetScaffoldState(
         bottomSheetState = bottomSheetState
     )
 
     LaunchedEffect(key1 = viewModel.isSignUpApiFinished) {
-        //이메일 인증 클릭 성공시 모달 올라오게.
-        if (viewModel.isSignUpApiFinished){
+        if (viewModel.isSignUpApiFinished) {
             bottomSheetState.show()
             delay(3000)
             viewModel.isSignUpApiFinished = false
@@ -77,7 +79,7 @@ fun TabletSignUpRoute(
     }
 
     LaunchedEffect(key1 = viewModel.errorCode) {
-        if (viewModel.errorCode >= 400){
+        if (viewModel.errorCode >= 400) {
             delay(2000)
             viewModel.errorCode = 200
         }
@@ -85,6 +87,9 @@ fun TabletSignUpRoute(
 
     TabletSignUpScreen(
         navController = navController,
+        scaffoldState = scaffoldState,
+        screenHeightDp = screenHeightDp,
+        passwordFocusRequester = passwordFocusRequester,
         viewModel = viewModel,
         horizontalPadding = if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 350 else 100,
         detectTapGestures = { keyboardController?.hide() },
@@ -92,6 +97,10 @@ fun TabletSignUpRoute(
         onSubmitEmail = {
             keyboardController?.hide()
             viewModel.getUserEmailCheck(viewModel.userEmail, navController)
+        },
+        onInputComplete = {
+            keyboardController?.hide()
+            viewModel.requestRegister(it, navController)
         }
     )
 }
@@ -100,163 +109,190 @@ fun TabletSignUpRoute(
 @Composable
 internal fun TabletSignUpScreen(
     navController: NavController,
+    scaffoldState: BottomSheetScaffoldState,
+    screenHeightDp: Int,
+    passwordFocusRequester: FocusRequester,
     viewModel: SignUpViewModel,
     horizontalPadding: Int,
     detectTapGestures: () -> Unit,
     onBackPressed: () -> Unit,
-    onSubmitEmail: () -> Unit
+    onSubmitEmail: () -> Unit,
+    onInputComplete: (String) -> Unit
 ) {
-    LinkedOutTheme {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Black),
-                    title = {},
-                    navigationIcon = {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "arrow back",
-                            modifier = Modifier
-                                .clickable { onBackPressed() }
-                                .padding(start = 10.dp)
-                                .size(30.dp),
-                            tint = Color.White
-                        )
-
-                    }
-                )
-            },
-            modifier = Modifier.pointerInput(Unit) {
-                detectTapGestures(onTap = {
-                    detectTapGestures()
-                })
-            },
-            content = {
-                if (viewModel.isLoading) {
-                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                        CircularProgressIndicator(color = LinkedInColor)
-                    }
-                }
-
-                Box {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(it)
-                            .padding(horizontal = horizontalPadding.dp)
-                    ) {
-                        Spacer(modifier = Modifier.height(50.dp))
-                        Text(
-                            text = "이메일로 가입하기",
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.SemiBold,
-                            modifier = Modifier.padding(start = 16.dp),
-                            color = Color.White
-                        )
-                        Text(
-                            text = "회원 서비스 이용을 위해 회원가입을 해주세요.",
-                            fontSize = 16.sp,
-                            modifier = Modifier.padding(start = 16.dp, bottom = 32.dp),
-                            color = Color(0xFF919191)
-                        )
-
-                        LoginTextFields(navController = navController)
-
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text(
-                            text = "*비밀번호는 영문(대소문자), 특수문자, 숫자 포함 8~12자를 조합해 주세요.",
-                            fontSize = 10.5.sp,
-                            color = LinkedInColor,
-                            modifier = Modifier.padding(horizontal = 30.dp)
-                        )
-
-                        Button(
-                            shape = RoundedCornerShape(10.dp),
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = LinkedInColor,
-                                disabledContainerColor = Color(0xFF868686)
-                            ),
-                            enabled = !viewModel.userEmailError && viewModel.userPw.isNotEmpty(),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(100.dp)
-                                .padding(start = 20.dp, end = 20.dp, top = 50.dp),
-                            onClick = {
-                                onSubmitEmail()
-                            }
-                        ) {
-                            Text(text = "인증 메일 보내기", color = Color.Black)
-                        }
-                    }
-                }
-
-                AnimatedVisibility(
-                    visible = viewModel.isSignUpApiFinished,
-                    enter = fadeIn(
-                        animationSpec = tween(
-                            durationMillis = 500,
-                            easing = FastOutSlowInEasing
-                        )
-                    ),
-                    exit = fadeOut(
-                        animationSpec = tween(
-                            durationMillis = 500,
-                            easing = LinearEasing
-                        )
-                    )
-                ) {
-                    Box(modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Black.copy(0.7f))
-                        .clickable(enabled = false) { }) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(bottom = 20.dp), contentAlignment = Alignment.BottomCenter
-                        ) {
-                            SendSignUpFinishedAlert(
-                                { viewModel.isSignUpApiFinished = false },
-                                "이메일 주소로 인증 메일이 발송됐습니다.",
-                                "링크를 클릭해 회원가입을 완료해주세요 !!"
-                            )
-                        }
-                    }
-                }
-
-                AnimatedVisibility(
-                    visible = viewModel.isSignUpApiFinished,
-                    enter = fadeIn(
-                        animationSpec = tween(
-                            durationMillis = 500,
-                            easing = FastOutSlowInEasing
-                        )
-                    ),
-                    exit = fadeOut(
-                        animationSpec = tween(
-                            durationMillis = 500,
-                            easing = LinearEasing
-                        )
-                    )
-                ) {
-                    Box(modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Black.copy(0.7f))
-                        .clickable(enabled = false) { }) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(bottom = 20.dp)
-                                .navigationBarsPadding(), contentAlignment = Alignment.BottomCenter
-                        ) {
-                            SendSignUpFinishedAlert(
-                                { viewModel.isSignUpApiFinished = false },
-                                "에러가 발생했습니다.",
-                                "사용중인 이메일이거나 비밀번호 조합 오류입니다."
-                            )
-                        }
-                    }
-                }
-            }
+    BottomSheetScaffold(
+        sheetContainerColor = Color(0xFF191919),
+        scaffoldState = scaffoldState,
+        sheetContent = {
+            SignUpBottomSheetContent(
+                viewModel = viewModel,
+                navController = navController,
+                onInputComplete = onInputComplete
+            )
+        },
+        sheetPeekHeight = (0.8 * screenHeightDp).dp
+    ) {
+        SignUpContent(
+            horizontalPadding = horizontalPadding,
+            detectTapGestures = detectTapGestures,
+            onBackPressed = onBackPressed,
+            viewModel = viewModel,
+            onSubmitEmail = onSubmitEmail,
+            passwordFocusRequester = passwordFocusRequester
         )
+    }
+
+    AnimatedVisibility(
+        visible = viewModel.isSignUpApiFinished,
+        enter = fadeIn(
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = FastOutSlowInEasing
+            )
+        ),
+        exit = fadeOut(
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = LinearEasing
+            )
+        )
+    ) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(0.7f))
+            .clickable(enabled = false) { }) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 20.dp), contentAlignment = Alignment.BottomCenter
+            ) {
+                SendSignUpFinishedAlert(
+                    { viewModel.isSignUpApiFinished = false },
+                    "이메일 주소로 인증 메일이 발송됐습니다.",
+                    "링크를 클릭해 회원가입을 완료해주세요 !!"
+                )
+            }
+        }
+    }
+    SignUpFinishedAlert(viewModel)
+}
+
+@Composable
+private fun SignUpContent(
+    horizontalPadding: Int,
+    detectTapGestures: () -> Unit,
+    onBackPressed: () -> Unit,
+    viewModel: SignUpViewModel,
+    onSubmitEmail: () -> Unit,
+    passwordFocusRequester: FocusRequester
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) { detectTapGestures { detectTapGestures() } }
+            .padding(horizontal = horizontalPadding.dp)
+    ) {
+        Column {
+            TabletDrawableTopBar(title = "회원가입", isBack = true, onBackPressed)
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(
+                text = "이메일로 가입하기",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(start = 16.dp),
+                color = Color.White
+            )
+            Text(
+                text = "회원 서비스 이용을 위해 회원가입을 해주세요.",
+                fontSize = 16.sp,
+                modifier = Modifier.padding(start = 16.dp, bottom = 32.dp),
+                color = Color(0xFF919191)
+            )
+            EmailTextField(viewModel, passwordFocusRequester)
+            PwTextField(viewModel, passwordFocusRequester)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "*비밀번호는 영문(대소문자), 특수문자, 숫자 포함 8~12자를 조합해 주세요.",
+                fontSize = 10.5.sp,
+                color = LinkedInColor,
+                modifier = Modifier.padding(horizontal = 30.dp)
+            )
+            Button(
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = LinkedInColor,
+                    disabledContainerColor = Color(0xFF868686)
+                ),
+                enabled = !viewModel.userEmailError && viewModel.userPw.isNotEmpty(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(start = 20.dp, end = 20.dp, top = 50.dp),
+                onClick = {
+                    onSubmitEmail()
+                }
+            ) {
+                Text(text = "인증 메일 보내기", color = Color.Black)
+            }
+        }
+    }
+}
+
+@Composable
+private fun SignUpBottomSheetContent(
+    viewModel: SignUpViewModel,
+    navController: NavController,
+    onInputComplete: (String) -> Unit
+) {
+    Box {
+        Authentication_6_BottomModal(
+            { viewModel.getUserEmailCheck(viewModel.userEmail, navController) },
+            isError = viewModel.errorCode >= 400,
+            isTypedLastNumber = { list -> onInputComplete(list.joinToString("")) }
+        )
+        if (viewModel.isLoading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(color = LinkedInColor)
+            }
+        }
+    }
+}
+
+@Composable
+private fun SignUpFinishedAlert(viewModel: SignUpViewModel) {
+    AnimatedVisibility(
+        visible = viewModel.isSignUpApiFinished,
+        enter = fadeIn(
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = FastOutSlowInEasing
+            )
+        ),
+        exit = fadeOut(
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = LinearEasing
+            )
+        )
+    ) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(0.7f))
+            .clickable(enabled = false) { }) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 20.dp)
+                    .navigationBarsPadding(), contentAlignment = Alignment.BottomCenter
+            ) {
+                SendSignUpFinishedAlert(
+                    { viewModel.isSignUpApiFinished = false },
+                    "에러가 발생했습니다.",
+                    "사용중인 이메일이거나 비밀번호 조합 오류입니다."
+                )
+            }
+        }
     }
 }
