9b3a2d75746d910af616535e9e26105cc12afca8
kkh <kkhyungyung0@naver.com>
2024-09-04 23:16:36 +0900

    github action last test3

--- app/src/main/java/com/echoist/linkedout/page/home/HomePage.kt
+++ app/src/main/java/com/echoist/linkedout/page/home/HomePage.kt
@@ -110,7 +110,7 @@ import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Locale
-//github action last test3
+//github action last test344
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun HomePage(
