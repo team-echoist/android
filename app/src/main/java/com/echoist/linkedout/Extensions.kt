95918e1ca7dda25eba3ccd00a0d2499fcd0572c7
kkh <kkhyungyung0@naver.com>
2024-09-11 18:31:51 +0900

    fix : getCurrentRoute 추가
    Extensions.kt에 현재 route를 반환하는 함수를 추가하였습니다.

--- app/src/main/java/com/echoist/linkedout/Extensions.kt
+++ app/src/main/java/com/echoist/linkedout/Extensions.kt
@@ -1,7 +1,13 @@
 package com.echoist.linkedout
 
 import android.util.Patterns
+import androidx.navigation.NavController
 
 fun String.isEmailValid(): Boolean {
     return Patterns.EMAIL_ADDRESS.matcher(this).matches()
+}
+
+fun getCurrentRoute(navController: NavController): String? {
+    // 현재 BackStackEntry를 가져와서 경로를 반환
+    return navController.currentBackStackEntry?.destination?.route
 }
